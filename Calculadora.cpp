b72f63fb9205486607be79e001d4f4637d7e913f
Lazaro Bajo Colina <87947396+LazaroBajoColina@users.noreply.github.com>
2021-09-11 13:31:37 -0500

    Calculadora en c++
    Esta es una calculadora realizada en el editor de texto (Visual Studio Code), creo que aún  no esta acabada.

---
+++ Calculadora.cpp
@@ -0,0 +1,29 @@
+#inlude <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+ int n1,n2,n3, suma=0, resta=0, multiplicacion=0, division =0, modular=0;
+double porcentaje;
+float raiz=0;
+cout<<"/n Digita un número: "; cin>>n1;
+cout<<"/n Digita un número: "; cin>>n2;
+cout<<"/n Digita un número: "; cin>>n3;
+
+suma = n1 + n2 + n3;
+resta = n1 - n2 - n3;
+multiplicacion = n1 * n2 * n3;
+division = n1 / n2 / n3;
+modular = n1 % n2 % n3;
+porcentaje *= (porcentaje / 100);
+ sqrt ((n1,n2,n3));
+ 
+cout<<"/n El resultado de la suma es: "; cin>>suma;
+cout<<"/n El resultado de la resta es: "; cin>>resta;
+cout<<"/n El resultado de la multiplicación es: "; cin>> multiplicacion;
+cout<<"/n El resultado de la division es: "; cin>> division;
+cout<< "/n El resultado del modular es: "; cin>> modular;
+cout<<"/n El resultado del porcentaje es: "; cin>> porcentaje;
+cout<"/n El resultado de la raiz cuadrada es: "; cin>> raiz;
+
+    return 0;
+}
