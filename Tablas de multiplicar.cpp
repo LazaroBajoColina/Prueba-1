907b54afc01e4202fe7833f5b4c5c982179d174d
Lazaro Bajo Colina <87947396+LazaroBajoColina@users.noreply.github.com>
2021-09-14 21:41:04 -0500

    Add files via upload
    Este es un programa en el cuál se exponen las tablas de multiplicar en consola mediante el ciclo for

---
+++ Tablas de multiplicar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+  int N, i;
+    cout <<"Ingresa un número: "; cin>>N;
+    
+    for(int i=0; i<=10; i++){ 
+      
+      cout<<N<<"x"<<i<<"="<<N*i<<endl;
+      
+      }
+    
+  
+   
+    
+    
+    return 0;
+}
