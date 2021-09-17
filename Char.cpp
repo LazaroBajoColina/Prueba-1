ef63e0fdcaa3379badc0c4a4106a77d5c91c9c3f
Lazaro Bajo Colina <87947396+LazaroBajoColina@users.noreply.github.com>
2021-09-17 15:19:52 -0500

    Char
    Programa que muestra en consola la letra o frase que digitaste, tiene un fallo porque no tiene una función, este fallo es que cuando pones un espacio en una determinada frase no te muestra la totalidad de la frase, es decir se rompor por el cout.
    Ejemplo:
    Instagram privado (en el espacio que tiene la frase "Instagram privado" no muestra privado solo instagram)
    Este fallo se puede solucionar con una barra hacia abajo  "Instagram_privado".

---
+++ Char.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main()
+{
+  char CDTxT [100];
+  
+  cout<<"Digita una letra: ";cin>>CDTxT;
+    cout << "Esta es la letra que digitaste: ";
+     cout<<CDTxT<<endl;
+    return 0;
+    }
+   
