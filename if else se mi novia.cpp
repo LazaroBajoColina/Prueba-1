8fe452bc16f9ac4c0f17d110895db1e84bdbf152
Lazaro Bajo Colina <87947396+LazaroBajoColina@users.noreply.github.com>
2021-11-08 13:45:53 -0600

    If else
    Este es un programa en un ciclo if else para el 14 de febrero.

---
+++ if else se mi novia.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int en;
+ cout<<"\n Solo elije un 1 o 0"; 
+ cout<<"\n¿Quieres ser mi novia? ";cin>>en;
+ 
+  if (en == 1 ){
+    
+    cout<<"Sabia que dirías que si";
+    
+  }
+  else {
+  
+    cout<<"Bueno gracias";
+   
+  }
+  
+ 
+  
+  
+  return 0;
+  }
+  
