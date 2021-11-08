d246cbf799dd9b832eb0e4e77409c9fdef0121aa
Lazaro Bajo Colina <87947396+LazaroBajoColina@users.noreply.github.com>
2021-11-08 13:47:07 -0600

    Switch case ¿Quieres ser mi novia?
    Este es un programa en switch case para el 14 de febrero.

---
+++ Switch Case se mi novia.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  char en;
+  cout<<"Quieres ser mi novia,";cin>>en;
+   switch (en){
+  case 1: cout<<"Sabia que dirias que zi";break;
+  case 2: cout<<"Bueno gracias";break;
+  default: cout<<"f";break;
+  }
+  
+  return 0;
+  }
