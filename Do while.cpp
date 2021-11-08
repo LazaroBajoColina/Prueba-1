6fb9976fe14759d3986d76c1cf4d09c571367459
Lazaro Bajo Colina <87947396+LazaroBajoColina@users.noreply.github.com>
2021-11-08 13:49:49 -0600

    Do while juego.
    Pequeño juego hecho en  un ciclo do while, adivina el número que estoy pensando.

---
+++ Do while.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i, gis;
+   
+    do{ 
+     
+        cout<<"Ingresa el número que estoy pensando: "; cin>>gis;
+      
+  
+      } while (gis<=100 );
+      cout<<"\n Le atinaste";
+      
+       do{
+         
+         
+        
+       } while (gis<= 100);
+       cout<<"\n Si"
+     
+    return 0;
+}
