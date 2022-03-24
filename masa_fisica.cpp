5962e1c2eb64c8de40afff71a1ad1d8d62c3cf2a
Lazaro Bajo Colina <87947396+LazaroBajoColina@users.noreply.github.com>
2022-03-23 18:49:43 -0600

    Ejercicio de fisica
    El ejercicio se trata de calcular la masa y el kilo en base a los parametros proporcionados por el usuario y las variables

---
+++ masa_fisica.cpp
@@ -0,0 +1,16 @@
+// Ejercicio de fisica masa 
+#include<iostream>   
+using namespace std;
+int main() 
+{ 
+float m1, multiplicacion;
+float n = 9.81;
+
+cout<<"Ejercicio de fisica"; 
+ cout<<"\nIngrese el valor de la masa: ";cin>>m1;
+   multiplicacion= m1*n;
+   suma= m1+n; // Donde ocurre la operación aritmética
+    cout<<"\n La masa en newtons es:   n"<<multiplicacion,suma;
+    
+    return 0; 
+ } 
